2fe6331b10495236f0c3d030d7e6c7ef72587ffb
p.venhryn <p.venhryn@pozitiff.ua>
2020-10-21 00:38:46 +0300

    update_sds

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,16 +2,28 @@
 $(document).ready(function(){
   // console.log('1');
   var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  console.log(lazyImages.event);
   
   if ("IntersectionObserver" in window) {
       let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
-          let lazyImage = entry.target;
-          lazyImage.src = lazyImage.dataset.src;
-          lazyImage.srcset = lazyImage.dataset.srcset;
-          lazyImage.classList.remove("lazy");
-          lazyImageObserver.unobserve(lazyImage);
-      });
+        entries.forEach(function(entry) {
+          // console.log(entry, ' netry')
+          // console.log(entry.isIntersecting)
+            if (entry.isIntersecting) {
+              let lazyImage = entry.target;
+              // console.log(lazyImage, ' lazy ');
+              lazyImage.src = lazyImage.dataset.src;
+              lazyImage.srcset = lazyImage.dataset.srcset;
+              lazyImage.classList.remove("lazy");
+              lazyImageObserver.unobserve(lazyImage);
+            }else{
+              // console.log(entry);
+            }
+            // if (entry.intersectionRatio === 1) {
+            //   console.log(entry.target);
+            // }
+            // console.log(entry.target, ' target ');
+        });
       });
 
       lazyImages.forEach(function(lazyImage) {
