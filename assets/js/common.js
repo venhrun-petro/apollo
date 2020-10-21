1dfd8dc8580ffcc2712b1927b66f62a6cfaa22dc
p.venhryn <p.venhryn@pozitiff.ua>
2020-10-21 17:13:24 +0300

    add_iframe

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-  // console.log('1');
+  
   var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
  
   
@@ -46,6 +46,58 @@ $(document).ready(function(){
         // Possibly fall back to event handlers here
         console.log('img false')
     }
+
+
+    var lazyIframes = [].slice.call(document.querySelectorAll(".lazy-iframe"));
+
+    if ("IntersectionObserver" in window) {
+      let lazyIframeObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+              let lazyIframe = entry.target;
+              let frame = lazyIframe.dataset.iframe;
+
+              let createIframe = document.createElement("iframe");
+              createIframe.setAttribute("frameborder", "0");
+              createIframe.setAttribute("allow", "autoplay; fullscreen");
+              createIframe.setAttribute("allowfullscreen", "");
+              createIframe.setAttribute("src", frame);
+              lazyIframe.appendChild(createIframe);
+              // lazyIframe.classList.add(LazyClass);
+              lazyIframeObserver.unobserve(lazyIframe);
+            }
+        });
+      });
+
+      lazyIframes.forEach(function(lazyIframe) {
+        lazyIframeObserver.observe(lazyIframe);
+      });
+    } else {
+        // Possibly fall back to event handlers here
+        console.log('img ifrmae');
+    }
+
+
+
+    // var articleVideo = document.getElementById("article-play-modal");
+		// var articlePlay = document.getElementById("article-play");
+		// // DOM ready, take it away
+		// if(articlePlay){
+		// 	articlePlay.onclick = function(){
+		// 		articleVideo.classList.add('active');
+		// 		let createIframe = document.createElement("iframe");
+		// 		createIframe.setAttribute("frameborder", "0");
+		// 		createIframe.setAttribute("allow", "autoplay; fullscreen");
+		// 		createIframe.setAttribute("allowfullscreen", "");
+		// 		createIframe.setAttribute("src", articleVideo.dataset.src);
+		// 		articleVideo.children[0].appendChild(createIframe);
+		// 		articleVideo.children[0].children[0].onclick = function(){
+		// 			articleVideo.classList.remove('active');
+		// 			articleVideo.children[0].removeChild(articleVideo.children[0].children[1]);
+		// 		}; 
+		// 	};
+		// }
+
  
 }); 
  
